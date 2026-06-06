529e2bbd923f328e79139fb4047ae525ac2c2d35
Lautaro Gladysz <lautygg06@gmail.com>
2026-06-05 22:04:51 -0300

    Registro.js y Login.js

---
+++ vistas/iniciar_sesion/login.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("formLogin");
+const mensajeError = document.getElementById("mensajeError");
+
+form.addEventListener("submit", function (evento) {
+
+    evento.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    let usuarioEncontrado = null;
+
+    for (let i = 0; i < usuarios.length; i++) {
+
+        if (usuarios[i].email === email && usuarios[i].password === password) {
+            usuarioEncontrado = usuarios[i];
+            break;
+        }
+    }
+
+    if (usuarioEncontrado == null) {
+        mensajeError.textContent = "Email o contraseña incorrectos";
+        return;
+    }
+
+    mensajeError.textContent = "";
+
+    localStorage.setItem("usuarioLogueado", JSON.stringify(usuarioEncontrado));
+
+    window.location.href = "../index_logueado/index.html";
+});
---
+++ vistas/registrarse/registro.js
@@ -0,0 +1,51 @@
+const form = document.getElementById("formRegistro");
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const dni = document.getElementById("dni").value;
+    const nacimiento = document.getElementById("nacimiento").value;
+    const pasaporte = document.getElementById("pasaporte").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  let usuarioExistente = false;
+
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].email === email) {
+            usuarioExistente = true;
+        }
+    }
+
+    if (dni.length != 8) {
+        mensajeError.textContent = "El DNI debe tener 8 dígitos";
+        return;
+    }
+
+    if (usuarioExistente) {
+        mensajeError.textContent = "Ya existe una cuenta con ese correo";
+        return;
+    }
+
+    const nuevoUsuario = {
+       nombre: nombre,
+        apellido: apellido,
+        dni: dni,
+        nacimiento: nacimiento,
+        pasaporte: pasaporte,
+        email: email,
+        password: password
+    };
+
+    usuarios.push(nuevoUsuario);
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    alert("Usuario registrado correctamente");
+
+    window.location.href = "../../index.html";
+});
