37df90c0f1db220bbcf8a806333179373fd3f1a8
Lautaro Gladysz <lautygg06@gmail.com>
2026-06-09 01:16:16 -0300

    Arreglos

--- vistas/reserva_confirmada/reserva_confirmada.js
+++ vistas/reserva_confirmada/reserva_confirmada.js
@@ -4,10 +4,18 @@ if (!vuelo) {
     window.location.href = "/index.html";
     
 } else {
-    document.getElementById("ruta").textContent    = vuelo.origen + " → " + vuelo.destino;
-    document.getElementById("precio").textContent  = "$ " + vuelo.precio_total_usd + " USD";
-    document.getElementById("fecha").textContent   = vuelo.fecha_vuelo;
-    document.getElementById("hora").textContent    = vuelo.hora_vuelo + " - " + vuelo.llegada_estimada;
-    document.getElementById("escalas").textContent = vuelo.escalas === 0 ? "Directo" : vuelo.escalas;
-    document.getElementById("codigo").textContent  = vuelo.codigo_reserva;
+    document.getElementById("ruta").textContent = vuelo.origen + " → " + vuelo.destino;
+    document.getElementById("precio").textContent = "$ " + vuelo.precio_total_usd + " USD";
+    document.getElementById("fecha").textContent = vuelo.fecha_vuelo;
+    document.getElementById("hora").textContent = vuelo.hora_vuelo + " - " + vuelo.llegada_estimada;
+    document.getElementById("codigo").textContent = vuelo.codigo_reserva;
+
+    if (vuelo.escalas === 0) {
+        document.getElementById("label-escalas").textContent = "Sin escalas";
+        document.getElementById("escalas").textContent = "";
+    
+    } else {
+        document.getElementById("label-escalas").textContent = "Escalas: ";
+        document.getElementById("escalas").textContent = vuelo.escalas;
+    }
 }
