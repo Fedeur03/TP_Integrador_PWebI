23c2f103c9e750d26adcab16adb5ea5ff94cc7b0
Fedeur03 <fedeur09@gmail.com>
2026-06-13 17:51:34 -0300

    Actualiza usuario para guardar vuelos comprados

--- componentes/navbar.js
+++ componentes/navbar.js
@@ -73,7 +73,14 @@ marcarEnlaceActivo();
 
 const botonSalir = document.getElementById('boton-salir');
 
+
+
 botonSalir.addEventListener('click', function () {
+    const usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+    const listaUsuarios = JSON.parse(localStorage.getItem("usuarios"));
+    const nuevoArrayUsuarios = listaUsuarios.filter(usuario => usuario.email !== usuarioLogueado.email);
+    nuevoArrayUsuarios.push(usuarioLogueado);
+    localStorage.setItem('usuarios', JSON.stringify(nuevoArrayUsuarios));
     localStorage.removeItem('usuarioLogueado');
     window.location.href = '/index.html';
 })
